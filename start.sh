11e9c2f2e6d3c5a6b0d5e0cf7a352268fe1cb153
jacquline <jacqulinewangu@gmail.com>
2019-06-03 11:00:40 +0300

    added the database_url and the secret key

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='Rayb'
+export MAIL_USERNAME='jacqulinewangu@gmail.com'
+export MAIL_PASSWORD='0704588886'
+export SQLALCHEMY_DATABASE_URL='postgresql+psycopg2://jacquline:jaki@localhost/blog'
+
+python3.6 manage.py server
